5c7331b6758f27ecec2fcdb22ec2a15665f7823c
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-12-04 13:56:37 +0600

    mission accomplished 🔥🚀🔥

--- src/components/Dashboard/AdminProfile.tsx
+++ src/components/Dashboard/AdminProfile.tsx
@@ -125,7 +125,6 @@ const AdminProfile = () => {
                   <span className="font-semibold">User Name:</span>{" "}
                   {filteredUserAccount?.user}
                 </p>
-
                 <p className="text-[18px] sm:text-[20px] text-black my-2">
                   <span className="font-semibold">Full Name:</span>{" "}
                   {singleUser?.first_name} {singleUser?.last_name}
